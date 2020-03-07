eee81f1a73b362010dd2d0973fab4c848b3296a7
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-07 05:29:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
 if array.length == 1 
   return array.first
   elsif array.length == 2 
-  return array.join(" and " )
+  return array.join("and " )
   elsif array.length > 2
-  array[-1].insert(0," and " )
+  array[-1].insert(0,"and " )
   array.join(", ")
 end 
 end
